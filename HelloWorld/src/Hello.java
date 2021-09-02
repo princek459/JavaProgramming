f2c95d27204639d1ffb622dfbc7778be9793b40d
Prince kunzwa <princekunzwa@outlook.com>
2021-09-02 03:50:08 +0100

    hello world and Int example

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,6 +5,19 @@ public class Hello {
 		
 		System.out.println("Hello World");
 		
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		int mySecondNumber = 12;
+		int myThirdNumber = myFirstNumber * 2;
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+		int myLastOne = myTotal - 1000;
+		
+		
+		System.out.println("myFirstNumber is " + myFirstNumber);
+		System.out.println("mySecondNumber is " + mySecondNumber);
+		System.out.println("myThirdtNumber is " + myThirdNumber);
+		System.out.println("My total is " + myTotal);
+		System.out.println("My last Number is" + myLastOne);
+		
 	}
 
 }
