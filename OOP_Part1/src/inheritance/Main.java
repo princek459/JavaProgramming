c223ab444b95c5eb5bb2cc2f0912bc8f596bb06b
Prince kunzwa <princekunzwa@outlook.com>
2021-10-04 17:59:49 +0100

    inheritance initial push

---
+++ OOP_Part1/src/inheritance/Main.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Animal animal = new Animal("Animal", 1, 1, 5, 5);
+		
+		Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "Long Silky");
+		
+		dog.eat();
+
+	}
+
+}
