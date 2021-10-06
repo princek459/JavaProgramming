ed16d0781a7297a6a54be76ad51aaebc8748338a
Prince kunzwa <princekunzwa@outlook.com>
2021-10-06 19:41:32 +0100

    references vs value types initial push

---
+++ Arrays/src/referenceVSvalueTypes/Main.java
@@ -0,0 +1,15 @@
+package referenceVSvalueTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int myIntValue = 10;
+		int anotherIntValue = myIntValue;
+		
+		System.out.println("myIntValue" + myIntValue);
+		System.out.println("anotherIntValue = " + anotherIntValue);
+
+	}
+
+}
