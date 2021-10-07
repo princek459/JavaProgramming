f20e8688dc827ec5477e39f1106cc978d1febafc
Prince kunzwa <princekunzwa@outlook.com>
2021-10-07 02:09:03 +0100

    Minimum Element challenge push

---
+++ Arrays/src/minimumElementChallenge/Main.java
@@ -0,0 +1,59 @@
+package minimumElementChallenge;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter count:");
+		
+		int count = scanner.nextInt();
+		scanner.nextLine();
+		
+		int[] returnedArray = readIntegers(count);
+		
+		int returnedMin = findMin(returnedArray);
+		
+		System.out.println("min = " + returnedMin);
+
+	}
+	
+	private static int[] readIntegers(int count) {
+		
+		// initialises array and makes every element 0
+		int[] array = new int[count];
+		for(int i=1; i< array.length; i++) {
+			System.out.println("Enter a number:");
+			int number = scanner.nextInt();
+			scanner.nextLine();
+			array[i] = number;
+			
+		}
+		return array;
+		
+	}
+	
+	private static int findMin(int[] array) {
+		
+		int min = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < array.length; i++) {
+			
+			int value = array[i];
+			
+			if(value < min) {
+				min = value;
+			}
+		}
+		
+		return min;
+	}
+	
+	
+	
+
+}
