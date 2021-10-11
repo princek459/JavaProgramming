f2e8ec6401ed019e773058abe7cbbd5fce6d7bc7
Prince kunzwa <princekunzwa@outlook.com>
2021-10-11 14:18:00 +0100

    Autoboxing and Unboxing example final push

--- Arrays/src/autoboxingUnboxingExample/Main.java
+++ Arrays/src/autoboxingUnboxingExample/Main.java
@@ -20,22 +20,34 @@ public class Main {
 		
 		ArrayList<Integer> intArrayList = new ArrayList<Integer>();
 		
-		//loop to add to the array.
-		for(int i=0; i < 10; i++) {
-			// auto-boxing converting base primitive type int into an integer
-			intArrayList.add(Integer.valueOf(i));
-		}
-		
-		// loop to execute
-		for(int i=0; i < 10; i++) {
-			// annexing is taking from the object wrapper and converting back to primitive type.
-			System.out.println(i + " -> " + intArrayList.get(i).intValue());
-		}
+//		//loop to add to the array.
+//		for(int i=0; i < 10; i++) {
+//			// auto-boxing converting base primitive type int into an integer
+//			intArrayList.add(Integer.valueOf(i));
+//		}
+//		
+//		// loop to execute
+//		for(int i=0; i < 10; i++) {
+//			// annexing is taking from the object wrapper and converting back to primitive type.
+//			System.out.println(i + " -> " + intArrayList.get(i).intValue());
+//		}
 		
 		// Shorter way of Auto-boxing & Un-boxing.
-		Integer myIntValue = 56;
+		
+		Integer myIntValue = 56; // Integer.valueOf(56);
+		int myInt = myIntValue; // myIntValue.intValue();
 		
 		
+		ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+		for(double dbl=0.0; dbl <= 10.0; dbl += 0.5) {
+			// autoboxing
+			myDoubleValues.add(Double.valueOf(dbl));
+		}
+		// Iterate through the entire array
+		for(int i=0; i<myDoubleValues.size(); i++) {
+			double value = myDoubleValues.get(i).doubleValue();
+			System.out.println(i + " -> " + value);
+		}
 		
 		
 		
